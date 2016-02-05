ef1599b72dfa2ad6cfbca2980b5df5c8e4bbf322
Andrew C. Pacifico <andrewcpacifico@gmail.com>
2016-02-05 00:16:23 -0400

    fix: fixing the deleteCookie method

--- dist/jambo.js
+++ dist/jambo.js
@@ -147,19 +147,33 @@
             );
     };
 
-    /**
-     * Deletes a page cookie.
-     *
-     * @method deleteCookie
-     * @param {String} name The cookie name.
-     * @param {Object} options A key value pair set with method settings.
-     * @param {String} options.domain The domain to set.
-     * @param {String} options.path The path to set.
-     * @return {Void}
-     */
-    jambo.deleteCookie = function (name, options) {
-        this.setCookie(name, '', options);
-    };
+/**
+ * Deletes a page cookie.
+ *
+ * @method deleteCookie
+ * @param {String} cname The cookie name.
+ * @param {Object} options A key value pair set with method settings.
+ * @param {String} options.domain The domain to set.
+ * @param {String} options.path The path to set.
+ * @return {Void}
+ */
+jambo.deleteCookie = function (cname, options) {
+    options = options || {};
+
+    document.cookie = 
+        cname + '=;' +
+        (
+            options.domain !== undefined ? 
+                '; domain=' + options.domain : 
+                ''
+        ) +
+        (
+            options.path !== undefined ? 
+                '; path=' + options.path : 
+                ''
+        ) +
+        '; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
 
     /**
      * Format a monetary value on Brazilian currency.
