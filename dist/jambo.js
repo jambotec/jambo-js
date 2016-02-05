a39da0ebb1415ce2c12acfbb2efaab490c530a54
Andrew C. Pacifico <andrewcpacifico@gmail.com>
2016-02-05 00:04:52 -0400

    fix: error on deleteCookie method calling the setCookie

--- dist/jambo.js
+++ dist/jambo.js
@@ -152,11 +152,13 @@
      *
      * @method deleteCookie
      * @param {String} name The cookie name.
-     * @param {String} host The cookie host.
+     * @param {Object} options A key value pair set with method settings.
+     * @param {String} options.domain The domain to set.
+     * @param {String} options.path The path to set.
      * @return {Void}
      */
-    jambo.deleteCookie = function (name, host) {
-        this.setCookie(name, '', -1, '/', host);
+    jambo.deleteCookie = function (name, options) {
+        this.setCookie(name, '', options);
     };
 
     /**
